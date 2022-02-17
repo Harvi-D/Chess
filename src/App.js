ccdedc904f1864293bc3225f9cde159ddbc1105c
Dylan Harville <dr.dyloh@gmail.com>
2022-02-16 18:31:00 -0700

    basic board using react-chessboard; next: implement functionality and rules with chess.js package

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
 import GameBoard from './GameBoard';
 
 function App() {
   return (
     <div className='app-routes'>
-      <Switch>
-        <Route path='/'>
           <GameBoard />
-        </Route>
-      </Switch>
+        
       </div>
   );
 }
